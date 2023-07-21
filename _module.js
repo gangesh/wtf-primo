5a4c5a486459db10d6255e7830ff9be6fc3b4c0d
GM <gangeshmatta@gmail.com>
2023-07-22 00:55:17 +0530

    Update site

--- _module.js
+++ _module.js
@@ -6776,13 +6776,17 @@ function create_fragment$c(ctx) {
 					}
 				},
 				site_nav: [
-					{ "link": { "url": "/", "label": "About" } },
 					{
-						"link": { "url": "/", "label": "Product" }
+						"link": { "url": "/about", "label": "About" }
 					},
-					{ "link": { "url": "/", "label": "Why" } },
 					{
-						"link": { "url": "/", "label": "Pricing" }
+						"link": { "url": "/product", "label": "Product" }
+					},
+					{
+						"link": { "url": "/why", "label": "Why" }
+					},
+					{
+						"link": { "url": "/pricing", "label": "Pricing" }
 					}
 				],
 				cta: [
